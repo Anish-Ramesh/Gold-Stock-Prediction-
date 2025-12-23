adf96a0270e3268ce219974d6bcf776976ddced0
Anish <anishramesh005@gmail.com>
2025-12-23 09:59:33 +0530

    dep

--- app.py
+++ app.py
@@ -195,4 +195,6 @@ def index():
                            end_date=end_date)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
